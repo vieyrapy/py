83a61bda1410204dd681450ce9793eeb12eb3f8c
Hector Vieyra <latinotanato@gmail.com>
2020-04-16 23:00:45 -0400

    DB
    Prueba heroku

--- core/controller/Database.php
+++ core/controller/Database.php
@@ -3,7 +3,8 @@ class Database {
 	public static $db;
 	public static $con;
 	function Database(){
-		$this->user="root";$this->pass="";$this->host="localhost";$this->ddbb="bd";
+		$this->user="lprbqrsrkzwaoc";$this->pass="7f24e4d8ed460165c4df87da51f3825f0cd4f6adc286abcf38ae07bb88789676";$this->host="ec2-52-71-85-210.compute-1.amazonaws.com";$this->ddbb="dci536q9bk4pg1
+";
 	}
 
 	function connect(){
